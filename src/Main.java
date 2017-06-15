6ab0709de74fb502e486bb5ddfd2466ecb68f069
Pheoda <n.fondacci@gmail.com>
2017-06-15 08:49:42 +0200

    Ajout temps pour savoir durée d'exécution (5 minutes environ)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         Graph g = new Graph(new Reader("CommunesFrance.csv"));
 
         System.out.println("Start building.");
+        System.out.println(LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + ":" + LocalDateTime.now().getSecond());
         g.build();
         System.out.println("Done.");
+        System.out.println(LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute() + ":" + LocalDateTime.now().getSecond());
     }
 }
